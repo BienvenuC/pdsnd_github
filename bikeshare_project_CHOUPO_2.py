5e1a19c5e685dbf7be460290dcc4d6096b7f3a6c
Bienvenuc <chbienvenu@yahoo.fr>
2020-06-17 09:11:47 +0100

    docs: Remove hint on program line 19

--- bikeshare_project_CHOUPO_2.py
+++ bikeshare_project_CHOUPO_2.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!\n')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington).
     #city_list the city_list
     valid_input_city = ['chicago', 'new york city', 'washington','1','2','3']
     response1 = ""
